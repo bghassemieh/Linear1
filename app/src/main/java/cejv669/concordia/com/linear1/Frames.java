806f56eadfee52de9a9721b0557f35b5e29cf6df
B_GHASS <bghassemieh@yahoo.com>
2019-02-07 22:04:53 -0500

    Initial commit

---
+++ app/src/main/java/cejv669/concordia/com/linear1/Frames.java
@@ -0,0 +1,13 @@
+package cejv669.concordia.com.linear1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Frames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frames);
+    }
+}
